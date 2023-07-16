1f3a6982eb86d4c015c2cbae4a8e6d6d31044222
ionescualex99 <130495924+ionescualex99@users.noreply.github.com>
2023-07-16 11:25:23 -0400

    Add files via upload

---
+++ historique.sql
@@ -0,0 +1,29 @@
+CREATE TABLE IF NOT EXISTS player( 
+    fName VARCHAR(50) NOT NULL, 
+    lName VARCHAR(50) NOT NULL, 
+    userName VARCHAR(20) NOT NULL UNIQUE,
+    registrationTime DATETIME NOT NULL,
+    id VARCHAR(200) GENERATED ALWAYS AS (CONCAT(UPPER(LEFT(fName,2)),UPPER(LEFT(lName,2)),UPPER(LEFT(userName,3)),CAST(registrationTime AS SIGNED))),
+    registrationOrder INTEGER AUTO_INCREMENT,
+    PRIMARY KEY (registrationOrder)
+)CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci; 
+
+CREATE TABLE IF NOT EXISTS authenticator(   
+    passCode VARCHAR(255) NOT NULL,
+    registrationOrder INTEGER, 
+    FOREIGN KEY (registrationOrder) REFERENCES player(registrationOrder)
+)CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci; 
+
+CREATE TABLE IF NOT EXISTS score( 
+    scoreTime DATETIME NOT NULL, 
+    result ENUM('réussite', 'échec', 'incomplet'),
+    livesUsed INTEGER NOT NULL,
+    registrationOrder INTEGER, 
+    FOREIGN KEY (registrationOrder) REFERENCES player(registrationOrder)
+)CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci; 
+
+--3.Code pour créer 1 Vue (View) "history" 
+CREATE VIEW history AS
+SELECT s.scoreTime, p.id, p.fName, p.lName, s.result, s.livesUsed 
+FROM player p, score s
+WHERE p.registrationOrder = s.registrationOrder;
