7c466072abedf6be82e0642b9f9fa406bcb8a6dc
ionescualex99 <130495924+ionescualex99@users.noreply.github.com>
2023-07-25 02:26:56 -0400

    Add files via upload

--- About.php
+++ About.php
@@ -4,8 +4,24 @@
     <title>About</title>
 </head>
 <body>
-    <h1>Our amazing game</h1>
-    <p>description:</p>
+    <h1>Our amazing game:</h1>
+    <p>site Web PHP de jeux pour enfants, qui comprend les fonctionnalités suivantes :</p>
+    <p>1. Création de comptes utilisateur ou inscription (Sign-Up).</p> 
+    <p>2. Validation en temps réel des informations saisies par l’utilisateur dans le formulaire 
+de création de comptes utilisateur avec AJAX.</p> 
+<p>3. Connexion à un compte utilisateur existant (Sign-In).</p>
+<p>4. Déconnexion à un compte utilisateur connecté (Sign-Out et Time-Out).</p>
+<p>5. Modification du mot de passe d’un compte utilisateur existant.</p> 
+<p>6. Gestion de plusieurs niveaux d’un jeu de question/réponse qui s’enchainent.</p> 
+<p>7. Abandon d’une partie de jeu en cours.</p>
+<p>8. Affichage de l’historique du résultat de l’ensemble des parties de jeu.</p>
+<p>9. Manipulation d’une base de données MySQL pour enregistrer (insert), récupérer 
+(select) et modifier (update) les données collectées et générées par les 
+fonctionnalités précédentes.</p> 
+<p>10.Affichage personnalisé des fonctionnalités et ajout de fonctionnalités d’interactivité 
+et d’attraction additionnelles.</p>
+<p>11.Ajout de toute autre fonctionnalité optionnelle permettant de compléter, 
+personnaliser ou optimiser le site web avec PHP.</p>
 </body>
 </html>
 
